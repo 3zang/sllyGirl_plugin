f315358646fadafcd6194a8bc8ffdfb8fb5e180f
3zang <134578272+3zang@users.noreply.github.com>
2023-11-13 15:24:40 +0800

    Create qq_keep_alive.js

---
+++ download/qq_keep_alive.js
@@ -0,0 +1,17 @@
+/**
+ * @title QQ保活
+ * @create_at 2023-10-08 17:04:21
+ * @rule 滴滴
+ * @platform qq web cron wx
+ * @cron 30 9,17 * * *
+ * @description 🐒这个人很懒什么都没有留下。
+ * @author 三藏
+ * @version v1.0.0
+ */
+
+const sillyGirl =new SillyGirl()
+sillyGirl.push({ platform: "qq", groupCode: "你的QQ群号  ", content: "滴滴~" })//酷友撩妹
+sillyGirl.push({ platform: "qq", groupCode: "你的QQ群号  ", content: "滴滴~" })//爱传送
+sillyGirl.push({ platform: "qq", groupCode: "你的QQ群号", content: "滴滴~" })//三颗心脏
+//s.reply("滴滴")
+console.log("QQ消息测试~")
